ca0c379d9a2df7ce25f021a9de887f07a7a61c72
fawwazabrials <fawwazabrial.fas@gmail.com>
2023-07-14 20:19:39 +0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,7 @@
+const dotenv = require("dotenv").config();
+const app = require("./app");
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+});
